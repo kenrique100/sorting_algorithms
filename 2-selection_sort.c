572cbf8218585ef6d056acf5fd555abf04ac83c2
Kenrique <ngwakneri2016@gmail.com>
2023-10-19 14:22:00 +0100

    insertions sorting

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,7 +15,7 @@ for (i = 0; i < size; i++, head++)
 for (low = i, j = i; j < size; j++)
 if (array[j] < array[low])
 low = j;
-tmp = array[low];
+ tmp = array[low];
 array[low] = *head;
 *head = tmp;
 print_array(array, size);
