7429f46d32eda90206544f6a8638db1c4ca18cf1
Kenrique <ngwakneri2016@gmail.com>
2023-10-29 21:22:21 +0100

    updated commit

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdbool.h>
 
 void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
 void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
